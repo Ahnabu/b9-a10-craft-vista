f6e367dd98d697ab0cb2dbd125f86fe07576c753
Ahnabu <horairaabu013025@gmail.com>
2024-04-30 15:30:25 +0600

    table responsive

--- src/Sections/Home/AllArt/AllArt.jsx
+++ src/Sections/Home/AllArt/AllArt.jsx
@@ -25,22 +25,22 @@ const AllArt = () => {
                 <thead>
                     <tr>
                         
-                        <th>Name</th>
-                        <th>Category</th>
-                        <th>Price</th>
-                        <th>Stock Status</th>
-                        <th>Rating</th>
-                        <th>Details</th>
+                        <th className="lg:p-2">Name</th>
+                        <th className="lg:p-2 hide" >Category</th>
+                        <th className="lg:p-2">Price</th>
+                        <th className="hide lg:p-2">Stock Status</th>
+                        <th className="lg:p-2">Rating</th>
+                        <th className="lg:p-2">Details</th>
                     </tr>
                 </thead>
                 <tbody>
                     {arts.map(item => (
                         <tr key={item._id}>
-                            <td>{item.item_name}</td>
-                            <td>{item.subcategory_Name}</td>
-                            <td>{item.price}</td>
-                            <td>{item.stockStatus}</td>
-                            <td>{item.rating}</td>
+                            <td className="lg:p-2">{item.item_name}</td>
+                            <td className="hide p-2">{item.subcategory_Name}</td>
+                            <td className="lg:p-2">{item.price}</td>
+                            <td className="hide p-2">{item.stockStatus}</td>
+                            <td className="lg:p-2">{item.rating}</td>
                             <td><Link to={`/details/${item._id}`}><Button>View Details</Button> </Link>  </td>
                         </tr>
                     ))}
